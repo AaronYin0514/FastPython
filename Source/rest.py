048967a50e1bdddcf2e47b91029161381063a39b
Aaron <yinzhongzheng0514@zto.com>
2021-05-10 08:29:13 +0800

    linlin

--- Source/rest.py
+++ Source/rest.py
@@ -1,13 +1,2 @@
-a = {'name': '张三', 'age': 100, 'pp': [1, 2, 4]}
-b = a.copy()
-
-a['pp'].append(5)
-
-print(id(a)) # 4302864832
-print(id(b)) # 4302865024
-
-print(a) # {'name': '张三', 'age': 100, 'pp': [1, 2, 4, 5]}
-print(b) # {'name': '张三', 'age': 100, 'pp': [1, 2, 4, 5]}
-
-
-
+l = [(i, j) for i in range(2) for j in range(2)]
+print(l) # [(0, 0), (0, 1), (1, 0), (1, 1)]
